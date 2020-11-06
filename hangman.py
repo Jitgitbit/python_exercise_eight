a9cbd4b9bd635fdd70b13232276bc8b6aafd3034
Jitgitbit <rowowolf@gmail.com>
2020-11-06 15:44:06 +0100

    word list ready

--- hangman.py
+++ hangman.py
@@ -1,3 +1,8 @@
+import random
+
+word = random.choice(["Venus", "Mars", "Batman", "Pluto", "Moon", "Earth", "Jupiter"])
+print(word)
+
 name = input("Please enter your name here: ")
 print("Welcome" , name)
 print("-------------------")
